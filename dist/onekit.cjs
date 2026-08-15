89df83b0d8ce1f61c77b03ae36ff95e1a91049b5
hidecard <7.6858518e+07+hidecard@users.noreply.github.com>
2026-08-15 13:09:08 +0000

    build: refresh 3.1.10 artifacts

--- dist/onekit.cjs
+++ dist/onekit.cjs
@@ -3512,7 +3512,7 @@ const jsxDEV = h;
 // Main entry point with tree-shaking friendly exports
 // Core systems
 // Version info
-const VERSION = '3.1.9';
+const VERSION = '3.1.10';
 
 exports.API = API;
 exports.DependencyInjector = DependencyInjector;
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -15,4 +15,4 @@ export * from './modules/ssr';
 export * from './modules/template';
 export { OneKitWebComponent, registerWebComponent } from './modules/web-components';
 export { okjs, component, h, jsx, jsxDEV, Fragment } from './modules/jsx';
-export declare const VERSION = "3.1.9";
+export declare const VERSION = "3.1.10";
